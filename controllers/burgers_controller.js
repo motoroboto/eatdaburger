988b525fec2e8180471b0a0688d3168245912c17
motoroboto <robert.a.cullen@gmail.com>
2020-11-29 14:24:34 -0500

    basic structure of system up and running ...much tweaking to be done

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 // Create all our routes and set up logic within those routes where required.
 router.get("/", function (req, res) {
-    cat.all(function (data) {
+    burger.all(function (data) {
         var hbsObject = {
             burger: data
         };
