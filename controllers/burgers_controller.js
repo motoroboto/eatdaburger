851baea16265e20146f06f358b1f8956955b924f
motoroboto <robert.a.cullen@gmail.com>
2020-12-03 22:06:18 -0500

    design tweaks

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,17 +1,12 @@
 var express = require("express");
-
 var router = express.Router();
-
-// Import the model (burger.js) to use its database functions.
 var burger = require("../models/burger.js");
 
-// Create all our routes and set up logic within those routes where required.
 router.get("/", function (req, res) {
     burger.all(function (data) {
         var hbsObject = {
             burger: data
         };
-        console.log(hbsObject);
         res.render("index", hbsObject);
     });
 });
@@ -22,21 +17,16 @@ router.post("/api/burgers", function (req, res) {
     ], [
         req.body.burger_name, 0
     ], function (result) {
-        // Send back the ID of the new quote
         res.json({ id: result.insertId });
     });
 });
 
 router.put("/api/burgers/:id", function (req, res) {
     var condition = "id = " + req.params.id;
-
-    console.log("condition", condition);
-
     burger.update({
         devoured: req.body.devoured
     }, condition, function (result) {
         if (result.changedRows == 0) {
-            // If no rows were changed, then the ID must not exist, so 404
             return res.status(404).end();
         } else {
             res.status(200).end();
@@ -46,10 +36,8 @@ router.put("/api/burgers/:id", function (req, res) {
 
 router.delete("/api/burgers/:id", function (req, res) {
     var condition = "id = " + req.params.id;
-
     burger.delete(condition, function (result) {
         if (result.affectedRows == 0) {
-            // If no rows were changed, then the ID must not exist, so 404
             return res.status(404).end();
         } else {
             res.status(200).end();
@@ -57,5 +45,4 @@ router.delete("/api/burgers/:id", function (req, res) {
     });
 });
 
-// Export routes for server.js to use.
 module.exports = router;
--- public/assets/js/burger.js
+++ public/assets/js/burger.js
@@ -1,43 +1,34 @@
-// Make sure we wait to attach our handlers until the DOM is fully loaded.
 $(function () {
     $(".change-devoured").on("click", function (event) {
         var id = $(this).data("id");
         var newDevoured = $(this).data("newdevoured");
-
         var newDevouredState = {
             devoured: newDevoured
         };
 
-        // Send the PUT request.
         $.ajax("/api/burgers/" + id, {
             type: "PUT",
             data: newDevouredState
         }).then(
             function () {
                 console.log("changed devoured to", newDevoured);
-                // Reload the page to get the updated list
                 location.reload();
             }
         );
     });
 
     $(".create-form").on("submit", function (event) {
-        // Make sure to preventDefault on a submit event.
         event.preventDefault();
-
         var newburger = {
             burger_name: $("#ba").val().trim(),
-            // sleepy: $("[name=sleepy]:checked").val().trim()
         };
 
-        // Send the POST request.
         $.ajax("/api/burgers", {
             type: "POST",
             data: newburger
         }).then(
             function () {
                 console.log("created new burger");
-                // Reload the page to get the updated list
                 location.reload();
             }
         );
@@ -46,13 +37,11 @@ $(function () {
     $(".delete-burger").on("click", function (event) {
         var id = $(this).data("id");
 
-        // Send the DELETE request.
         $.ajax("/api/burgers/" + id, {
             type: "DELETE"
         }).then(
             function () {
                 console.log("deleted burger", id);
-                // Reload the page to get the updated list
                 location.reload();
             }
         );
